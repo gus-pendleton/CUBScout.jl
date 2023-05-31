eeeb98e3e82d4e5946e9145c63b966c9b72a2abe
gus=pendleton <guspendleton@gmail.com>
2023-05-31 14:32:20 -0400

    Build files?

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://gus-pendleton.github.io/CUBScout.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://gus-pendleton.github.io/CUBScout.jl/dev/)
 [![Build Status](https://github.com/gus-pendleton/CUBScout.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/gus-pendleton/CUBScout.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+### Codon Usage Bias (CUB) in Julia
---
+++ build/CUBScout.jl
@@ -0,0 +1,21 @@
+module CUBScout
+
+    using DataFrames
+    using FASTX
+    using DelimitedFiles
+    using BioSequences
+    using Artifacts
+    using Statistics
+
+    export codon_dict, make_codon_dict, codon_dict_path, default_codon_dict, altstart_codon_dict, example_data_path, find_seqs, seq_names, seq_descriptions
+    export b, enc, enc_p, mcb, milc, scuo, all_cub
+    export e, melp, cai, fop, gcb
+
+    include("codon_dict.jl")
+    include("accessory_functions.jl")
+    
+    include("CUB_measures.jl")
+    include("EXP_measures.jl")
+
+
+end
